44dd8e7d59b0d3aa4a43c39d2eed2c986e706550
James <jamesrishton03@gmail.com>
2024-11-18 11:54:42 +0000

    cont...

---
+++ src/sagittal_average/command.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from sagittal_average import sagittal_brain
+def process():
+    parser = ArgumentParser(description="Calculates the average for each sagittal-horizontal plane.",
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('file_input', nargs='?', default="brain_sample.csv",
+                help="Input CSV file with the results from scikit-brain binning algorithm.")
+    parser.add_argument('--file_output', '-o', default="brain_average.csv",
+                help="Name of the output CSV file.")
+    arguments = parser.parse_args()
+
+    print(sagittal_brain.run_averages(arguments.file_input, arguments.file_output))
+
+if __name__ == "__main__":
+    process()
